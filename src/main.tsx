01b1c8db9ecdc37ae41b5fb65b5d2aacb491ac2f
Kate Timofeeva <timofeeva.katerina2016@yandex.ru>
2025-07-14 19:09:46 +0300

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import store from './services/store.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/art-gallery/">
+  <BrowserRouter basename="/my-art-gallery/">
     <Provider store={store}>
       <App />
     </Provider>
